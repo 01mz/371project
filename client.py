7458960761846bbd862c6a91195d4a0cf1dec6eb
Michael Zheng <mz6141@gmail.com>
2022-07-28 19:17:34 -0700

    run GUI in client.py

--- client.py
+++ client.py
@@ -1,24 +1,28 @@
 from socket import *
 # from datetime import *
 
-serverName = 'localhost'
-serverPort = 65432
-
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-
-# start = datetime.now()
-clientSocket.send("hello".encode('utf-8'))
-print(clientSocket.recv(1024).decode('utf-8'))
-# end = datetime.now()
-
-# print(f"RTT = {end-start}")
-
-clientSocket.close()
+import view as gui
 
 
+def main():
+    serverName = 'localhost'
+    serverPort = 65432
+
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((serverName, serverPort))
+
+    # start = datetime.now()
+    clientSocket.send("hello".encode('utf-8'))
+    print(clientSocket.recv(1024).decode('utf-8'))
+    # end = datetime.now()
+
+    # print(f"RTT = {end-start}")
+
+    clientSocket.close()
+
+    # run GUI
+    gui.run()
 
 
-
-
-
+if __name__ == '__main__':
+    main()
