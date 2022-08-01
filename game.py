9c0bb945d395eb33b56bdc2da7193f06ef8c1534
ptdo <ptdo@sfu.ca>
2022-08-01 15:33:26 -0700

    fix

--- game.py
+++ game.py
@@ -43,6 +43,7 @@ class Game:
     def handleAction(self, player: Player, action: str, row: int, col: int):
         box = self.boxes[row][col]
         if action == Action.CHOOSE:
+            box.claimBy(player)
             command = f"{Action.CHOOSE} {row} {col} {player[1]}"
             self.broadcast(command)
         elif action == Action.CLAIM:
