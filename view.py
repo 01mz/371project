39353d300b7737f077456885ccad0c1fc8feed6c
Viet Hung Nguyen <vhn@sfu.ca>
2022-08-01 05:42:21 -0700

    Concurrency on server and client

--- view.py
+++ view.py
@@ -1,41 +1,43 @@
-# PyQt5 GUI based on https://realpython.com/python-pyqt-gui-calculator
-
 import sys
+from socket import socket
+from typing import Optional
 
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5.QtWidgets import QWidget
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QGridLayout
-from PyQt5.QtWidgets import QLabel
-from PyQt5.QtWidgets import QPushButton
-from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import (
+    QApplication,
+    QGridLayout,
+    QLabel,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
 
-nrows = 8
-ncols = 8
-gridBtnStyle = '''
+from constant import BOARD_SIZE, Action
+
+gridBtnStyle = """
     QPushButton {
         background-color : white;
         border: 1px solid black;
     }
-'''
-gridBtnStyleClicked = '''
+"""
+gridBtnStyleClicked = """
     QPushButton {
         background-color : yellow;
         border: 1px solid black;
     }
-'''
+"""
 
 
+# PyQt5 GUI based on https://realpython.com/python-pyqt-gui-calculator
 class GUI(QMainWindow):
-
     """Create a subclass of QMainWindow to setup the GUI"""
 
-    def __init__(self):
+    def __init__(self, player: Optional[socket] = None):
         """View initializer"""
         super().__init__()
         # Set main window properties
-        self.setWindowTitle('Deny and Conquer')
+        self.setWindowTitle("Deny and Conquer")
         # Set the central widget and general layout
         self.generalLayout = QVBoxLayout()
         self._centralWidget = QWidget(self)
@@ -44,6 +46,7 @@ class GUI(QMainWindow):
         # Create the display and the grid of buttons
         self._createDisplay()
         self._createButtons()
+        self.player = player
 
     def _createDisplay(self):
         """Create the display"""
@@ -58,12 +61,12 @@ class GUI(QMainWindow):
         """Create the buttons"""
 
         buttonsLayout = QGridLayout()
-        buttonsLayout.setSpacing(10)
+        buttonsLayout.setSpacing(5)
 
         # Create buttons and add them to the grid layout
-        for row in range(nrows):
-            for col in range(ncols):
-                btn = QPushButton(text=f'{row},{col}')
+        for row in range(BOARD_SIZE):
+            for col in range(BOARD_SIZE):
+                btn = QPushButton(text=f"{row},{col}")
                 btn.setFixedSize(60, 60)
                 btn.setStyleSheet(gridBtnStyle)
                 buttonsLayout.addWidget(btn, row, col)
@@ -71,23 +74,25 @@ class GUI(QMainWindow):
                 # Setup pressed (mouseDown) and released (mouseUp) events
                 # Note: setupMouseEvents creates a closure on btn, row, col (so that b, r, c are local vars)
                 def setupMouseEvents(b, r, c):
-                    btn.pressed.connect(
-                        lambda: self.onButtonPressed(b, r, c))
-                    btn.released.connect(
-                        lambda: self.onButtonReleased(b, r, c))
+                    btn.pressed.connect(lambda: self.onButtonPressed(b, r, c))
+                    btn.released.connect(lambda: self.onButtonReleased(b, r, c))
+
                 setupMouseEvents(btn, row, col)
 
         # Add buttonsLayout to the general layout
         self.generalLayout.addLayout(buttonsLayout)
 
-
     def onButtonPressed(self, btn: QPushButton, row: int, col: int):
-        self.setDisplayText(f'{row},{col} pressed')
-        btn.setStyleSheet(gridBtnStyleClicked)
+        self.setDisplayText(f"{row},{col} pressed")
+        if self.player is None: 
+            btn.setStyleSheet(gridBtnStyleClicked)
+            return
+        self.player.send(f"{Action.CHOOSE} {row} {col}".encode("utf-8"))
 
     def onButtonReleased(self, btn: QPushButton, row: int, col: int):
-        self.setDisplayText(f'{row},{col} released')
-
+        self.setDisplayText(f"{row},{col} released")
+        if self.player is None: return
+        self.player.send(f"{Action.RELEASE} {row} {col}".encode("utf-8"))
 
     def setDisplayText(self, text):
         self.display.setText(text)
@@ -103,5 +108,5 @@ def run():
     sys.exit(app.exec_())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
