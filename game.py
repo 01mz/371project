41068363aab850c128271f2bd97db5d2498caea5
Viet Hung Nguyen <vhn@sfu.ca>
2022-08-03 10:11:05 -0700

    Use one lock for all the game

--- game.py
+++ game.py
@@ -11,24 +11,20 @@ class Box:
     def __init__(self):
         self.claim: Optional[Player] = None
         self.hold: Optional[Player] = None
-        self.lock = Lock()
 
     def canBeClaim(self, player: Player):
-        with self.lock:
-            if self.hold != player: return False
-            self.claim = player
-            self.hold = None
-            return True
+        if self.hold != player: return False
+        self.claim = player
+        self.hold = None
+        return True
 
     def canBeHeld(self, player: Player):
-        with self.lock:
-            if self.hold is not None: return False
-            self.hold = player
-            return True
+        if self.hold is not None: return False
+        self.hold = player
+        return True
 
     def release(self):
-        with self.lock:
-            self.hold = None
+        self.hold = None
 
 
 class Game:
@@ -36,6 +32,7 @@ class Game:
         self.new = True
         self.players = []
         self.boxes: List[List[Box]] = []
+        self.lock = Lock()
         for _ in range(BOARD_SIZE):
             row: List[Box] = []
             for _ in range(BOARD_SIZE):
@@ -52,19 +49,19 @@ class Game:
         self.players.remove(player)
 
     def isPlaying(self):
-        return self.new or len(self.players) > 0
+        with self.lock:
+            return self.new or len(self.players) > 0
 
     def handleAction(self, player: Player, action: str, row: int, col: int):
-        box = self.boxes[row][col]
-        if action == Action.CHOOSE:
-            if box.canBeHeld(player):
+        with self.lock:
+            box = self.boxes[row][col]
+            if action == Action.CHOOSE and box.canBeClaim(player):
                 self.broadcast(f"{Action.CHOOSE} {row} {col} {player[1]}")
-        elif action == Action.CLAIM:
-            if box.canBeClaim(player):
+            elif action == Action.CLAIM and box.canBeClaim(player):
                 self.broadcast(f"{Action.CLAIM} {row} {col} {player[1]}")
-        elif action == Action.RELEASE:
-            box.release()
-            self.broadcast(f"{Action.RELEASE} {row} {col} {player[1]}")
+            elif action == Action.RELEASE:
+                box.release()
+                self.broadcast(f"{Action.RELEASE} {row} {col} {player[1]}")
 
     def broadcast(self, message: str):
         for player in self.players:
